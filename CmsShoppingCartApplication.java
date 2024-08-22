febac6c682491a41164c4d3eb4e0ad32f0735029
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 12:51:04 -0400

    Update CmsShoppingCartApplication.java

--- CmsShoppingCartApplication.java
+++ CmsShoppingCartApplication.java
@@ -1,4 +1,4 @@
-package com.vojislavk.cmsshoppingcart;
+package com.shoppingcart.models.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
