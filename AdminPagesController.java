133197f99f863550ef3aa52c547ea7d06cfe16ec
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 12:53:18 -0400

    Update AdminPagesController.java

--- AdminPagesController.java
+++ AdminPagesController.java
@@ -3,8 +3,8 @@ package com.shoppingcart.models.data;
 import java.util.List;
 import java.util.Optional;
 
-import com.vojislavk.restapi.models.PageRepository;
-import com.vojislavk.restapi.models.data.Page;
+import com.restapi.models.PageRepository;
+import com.restapi.models.data.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
