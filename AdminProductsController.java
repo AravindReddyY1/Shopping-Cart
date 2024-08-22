366024b2273302c1a373d602266a9219728e93a5
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 12:54:03 -0400

    Update AdminProductsController.java

--- AdminProductsController.java
+++ AdminProductsController.java
@@ -7,12 +7,12 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
-import com.vojislavk.cmsshoppingcart.models.CategoryRepository;
-import com.vojislavk.cmsshoppingcart.models.ProductRepository;
-import com.vojislavk.cmsshoppingcart.models.data.Category;
-import com.vojislavk.cmsshoppingcart.models.data.Product;
+import com.shoppingcart.models.CategoryRepository;
+import com.shoppingcart.models.ProductRepository;
+import com.shoppingcart.models.data.Category;
+import com.shoppingcart.models.data.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
