3c66987610d8713333305f6d24dcc18822c3507e
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 12:52:13 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,11 +3,11 @@ package com.shoppingcart.models.data;
 import java.util.Arrays;
 import java.util.Collection;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
