8ce778228bad663ee43ac50f1929f376b3c39215
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 13:05:07 -0400

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.vojislavk.cmsshoppingcart.security;
+package com.shoppingcart.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
