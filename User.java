d1d020d261d8f02d8a0fa3e76769424667780779
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 13:06:03 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,17 +1,17 @@
-package com.vojislavk.cmsshoppingcart.models.data;
+package com.shoppingcart.models.data;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
