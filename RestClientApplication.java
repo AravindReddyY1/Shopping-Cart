55846c17b38d0c030b5a6005f07c1e457e11271d
AravindReddyY1 <aravindkr685@gmail.com>
2024-08-22 13:04:33 -0400

    Update RestClientApplication.java

--- RestClientApplication.java
+++ RestClientApplication.java
@@ -1,4 +1,4 @@
-package com.vojislavk.restclient;
+package com.restclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
